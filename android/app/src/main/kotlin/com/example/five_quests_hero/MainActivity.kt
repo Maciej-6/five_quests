7d345c0b90152400d20544e8ea0b01287296d7e0
Maciej-6 <maciej.gauza@gmail.com>
2024-05-08 19:28:24 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/five_quests_hero/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.five_quests_hero
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(title: '5 Quests Hero'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isChecked = false;
+  // bool checkBoxValue1 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
